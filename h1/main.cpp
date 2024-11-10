6d4561b2aea10eee0ab0a8afd1e2e8c54b00e3f0
jaavil <jaakko.vilmi@gmail.com>
2024-11-10 08:19:44 +0200

    vaihe2

--- h1/main.cpp
+++ h1/main.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
+#include <iomanip>
+
+void calcSum(int a, int b) {
+    int summa = a + b;
+    std::cout << "Summa: " << summa << std::endl;
+}
+
+void calcDiv(int a, int b) {
+    if (b == 0) {
+        std::cerr << "virheilimootus. Ei nollaa jakajaksi." << std::endl;
+        return;
+    }
+    double osamaara = static_cast<double>(a) / b;
+    std::cout << "Osamäärä: " << std::fixed << std::setprecision(2) << osamaara << std::endl;
+}
 
 int main() {
     int a, b;
 
-    std::cout << "Anna kokonaisluvut (a ja b): ";
+    std::cout << "Anna kokonaisluvut(a ja b): ";
     std::cin >> a >> b;
+
+    calcSum(a, b);
+    calcDiv(a, b);
 
     return 0;
 }
